462fd7047056456ff3bd27523c5e81fb56fe64cc
4n5raaaan <pefu0920@gmail.com>
2020-05-08 17:27:34 +0900

    課題実施

--- src/main/java/q005/Q005.java
+++ src/main/java/q005/Q005.java
@@ -1,5 +1,10 @@
 package q005;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Q005 データクラスと様々な集計
  *
@@ -16,19 +21,47 @@ package q005;
  * 作業時間は "xx時間xx分" の形式にしてください。
  * また、WorkDataクラスは自由に修正してください。
  *
-[出力イメージ]
-部長: xx時間xx分
-課長: xx時間xx分
-一般: xx時間xx分
-Z-7-31100: xx時間xx分
-I-7-31100: xx時間xx分
-T-7-30002: xx時間xx分
-（省略）
-194033: xx時間xx分
-195052: xx時間xx分
-195066: xx時間xx分
-（省略）
+ [出力イメージ]
+ 部長: xx時間xx分
+ 課長: xx時間xx分
+ 一般: xx時間xx分
+ Z-7-31100: xx時間xx分
+ I-7-31100: xx時間xx分
+ T-7-30002: xx時間xx分
+ （省略）
+ 194033: xx時間xx分
+ 195052: xx時間xx分
+ 195066: xx時間xx分
+ （省略）
  */
 public class Q005 {
+    /**
+     * データファイルを開く
+     * resources/q005/data.txt
+     */
+    private static InputStream openDataFile() {
+        return Q005.class.getResourceAsStream("data.txt");
+    }
+
+    public static void main(String[] args) {
+        try {
+            WorkData wd = new WorkData();
+            String header = "社員番号,部署名,役職名,P-CODE,作業時間";
+            String delimit = "\\,";
+            InputStreamReader is = new InputStreamReader(openDataFile(), "utf-8");
+            BufferedReader br = new BufferedReader(is);
+            String ln;
+            while ((ln = br.readLine()) != null) {
+                if (header.equals(ln)) {
+                    continue;
+                }
+                String[] datas = ln.split(delimit);
+                wd.setData(datas[0], datas[2], datas[3], Integer.parseInt(datas[4]));
+                wd.aggregateData();
+            }
+            wd.outData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-// 完成までの時間: xx時間 xx分
